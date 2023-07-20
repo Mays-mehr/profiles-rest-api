00aed461388fc70e36aeef3a78e5c0621a351df4
mays <mays.mehr@yahoo.com>
2023-07-20 23:24:42 +0000

    rest is added to the project

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #"rest_framework",
-    #"rest_framework.authtoken",
-    "profiles_api",
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_api',
 
 ]
 
