14c7bd404564df6c1e10aae4495b225f5fa16f77
mays <mays.mehr@yahoo.com>
2023-07-22 02:04:30 +0000

    added search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers , models , permissions
 
@@ -105,7 +106,6 @@ class HelloViewSet(viewsets.ViewSet):
 
 
 
-
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating, creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
@@ -113,6 +113,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication , )
     permission_classes = (permissions.UpdateOwnProfile , )
+
+    filter_backends = (filters.SearchFilter , )
+    search_fields = ('name' , 'email')
 
 
 
