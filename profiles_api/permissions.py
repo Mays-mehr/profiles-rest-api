e1ad0e1115937cef59ebb90fe41aeb940a4733cc
mays <mays.mehr@yahoo.com>
2023-07-22 15:43:52 +0000

    added permission to feeds

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -5,12 +5,24 @@ class UpdateOwnProfile(permissions.BasePermission):
     """allow user to edit their own profile"""
 
     def has_object_permission(self, request, view, obj):
-        """check user who is trying to edit the profile"""
+        """check the user who is trying to edit the profile"""
         
         if request.method in permissions.SAFE_METHODS:
             return True
         
         return obj.id == request.user.id
+    
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """allow user to edit their own profile"""
+
+    def has_object_permission(self, request, view, obj):
+        """check the user who is trying to update the status the profile"""
+        
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id == request.user.id   
     
 
     
